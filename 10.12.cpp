b0416c374629553049fc6054866514c4143c264c
taylor <huangqihyper@gmail.com>
2017-02-02 09:28:21 -0500

    12 charpter

---
+++ 10.12.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "10.10.h"
+#include <cctype>
+
+int main(){
+	using namespace std;
+	Stack st;
+	char ch;
+	unsigned long po;
+	cout << "Please enter A to add a purchase order,\n"
+		<< "P to process a PO,or Q to quit.\n";
+	while(cin>>ch&&toupper(ch)!='Q'){
+		while(cin.get() != '\n'){	
+			continue;
+		}
+		if (!isalpha(ch)){
+			cout << '\a';
+			continue;
+		}
+		switch(ch){
+			case 'A':
+			case 'a':cout<< "Enter a po number to add:";
+				 cin >> po;
+				 if(st.isfull())
+					cout << "stack already full \n";
+				 else
+					st.push(po);
+				 break;
+			case 'P':
+			case 'p':if(st.isempty())
+					cout << "stack already empty\n";
+				 else{
+					st.pop(po);
+					cout << "Po #" << po << " popped\n";
+	
+				 }
+				 break;
+		}
+		cout << "Please enter A to add a puchase order,\n"
+			<< " P to process a  PO or Q to quit";
+	}
+	cout << "Bye\n";
+	return 0;
+}
