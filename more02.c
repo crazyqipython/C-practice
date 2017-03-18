82e95ddd4e518bb50a1e4960b00b6dd4332305fb
taylor <huangqihyper@gmail.com>
2017-03-17 23:33:31 -0400

    who impl

--- more02.c
+++ more02.c
@@ -21,9 +21,36 @@ int main(int ac, char * av[]){
 }
 
 void do_more(FILE * fp){
-	
+	char line[LINELEN];
+	int num_of_lines =0;
+	int see_more(FILE *),reply;
+	FILE * fp_tty;
+	fp_tty=fopen("/dev/tty","r");
+	if(fp_tty==NULL)
+		exit(1);
+	while(fgets(line,LINELEN,fp)){
+		if(num_of_lines==PAGELEN){
+			reply=see_more(fp_tty);
+			if(rep==0)
+				break;
+			num_of_lines ~=reply;
+		}
+		if(fputs(line,stdout)==EOF)
+			exit(1);
+		num_of_lines++;
+	}	
 }
 
 int see_more(FILE * cmd){
+	int c;
+	printf("\033[7mmore?\033[m");
+	while(c=getc(cmd)!=EOF){
+		if(c=='q')
+			return 0;
+		if(c==' ')
+			return PAGELEN;
+		if(c=='\n')
+			return 1;
+	}
 	return 0;
 }
