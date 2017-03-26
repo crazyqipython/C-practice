7bfe3841aff532436a8a3e557ae25fd0a0bf234b
taylor <huangqihyper@gmail.com>
2017-03-26 08:15:21 -0400

    signal call

---
+++ sigcall.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <signal.h>
+
+int main(){
+	void f(int);
+	int i;
+	signal(SIGINT,f);
+	for(i=0;i<5;i++){
+		printf("hello\n");
+		sleep(1);
+	}
+}
+
+void f(int s){
+	printf("hahaha\n");
+}
